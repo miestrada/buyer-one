cb67c74ada9605cd38b69c366e1f7dca29bd4a08
Miguel <miguel@dev>
2020-03-31 16:32:38 +0200

    Prepare for tests

---
+++ tests/API/BuyerV1Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\API;
+
+use PHPUnit\Framework\TestCase;
+
+class BuyerV1Test extends TestCase
+{
+
+    public function testTrue() {
+        $this->assertTrue(true);
+    }
+
+}
